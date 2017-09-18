92c56aba0f1a5a7174dfa2cb23146d539d47cd71
Stefan Feuerhahn <stefan.feuerhahn@younicos.com>
2017-09-18 15:18:49 +0200

    removed duplicate files that were mistakenly copied when repository was moved to github

--- src/asn1/iso-presentation-layer/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm ../../src/main/java-gen/org/openmuc/josistack/internal/presentation/asn1/*.java
-
-jasn1-compiler -o ../../src/main/java-gen/ -p org.openmuc.josistack.internal.presentation -f iso-presentation-layer.asn
--- src/asn1/mms/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -r ../../src/main/java-gen/*
-
-jasn1-compiler -o "../../src/main/java-gen" -p "org.openmuc.openiec61850.internal.mms" -f mms.asn
