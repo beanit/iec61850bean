bbc98a065a8977f7ca1629450c4f2f10551cb682
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2019-06-13 15:36:25 +0200

    moved all start scripts to bin folder

--- bin/openiec61850-console-client.bat
+++ bin/openiec61850-console-client.bat
@@ -1,6 +1,6 @@
 ::BATCH file for windows
 
 set BATDIR=%~dp0
-set LIBDIR=%BATDIR%..\..\build\libs-all
+set LIBDIR=%BATDIR%..\build\libs-all
 
 java  -Djava.ext.dirs=%LIBDIR% com.beanit.openiec61850.app.ConsoleClient %*
