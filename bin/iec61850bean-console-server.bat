48531ffd8d3bf5aa37085a1f0afb5746a424b99f
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2020-06-30 16:31:26 +0200

    windows run scripts now support white space in paths

--- bin/iec61850bean-console-server.bat
+++ bin/iec61850bean-console-server.bat
@@ -1,6 +1,6 @@
 ::BATCH file to windows
 
 set BATDIR=%~dp0
-set LIBDIR=%BATDIR%..\build\libs-all
+set LIBDIR="%BATDIR%..\build\libs-all"
 
 java -Dlogback.configurationFile=logback.xml -Djava.ext.dirs=%LIBDIR% com.beanit.iec61850bean.app.ConsoleServer %*
